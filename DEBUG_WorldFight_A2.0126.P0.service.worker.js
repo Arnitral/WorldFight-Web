1e79eac9f8a0c89a5ba783f6c8a70c33ac799067
ArnItraL <186834555+Arnitral@users.noreply.github.com>
2026-03-12 21:16:31 +0200

    fullscrean sys

--- DEBUG_WorldFight_A2.0126.P0.service.worker.js
+++ DEBUG_WorldFight_A2.0126.P0.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1773244187|10387095052';
+const CACHE_VERSION = '1773342784|206224727';
 /** @type {string} */
 const CACHE_PREFIX = 'World Fight Offl-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
